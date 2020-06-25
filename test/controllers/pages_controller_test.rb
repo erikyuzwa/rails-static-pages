de36ec31d341dbf0ad4de9a35c8850804b1e4cf7
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-25 17:31:03 -0600

    add root route and test coverage

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,9 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get home_url
     assert_response :success
   end
+
+  test "root route should get the home page" do
+    get root_url
+    assert_response :success
+  end
 end
