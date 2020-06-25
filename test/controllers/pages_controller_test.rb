133623910aa767a6e323f78fc8eea691ae6f9914
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-25 17:19:56 -0600

    added int test for home page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get the home page" do
+    get home_url
+    assert_response :success
+  end
 end
