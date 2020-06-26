ac2aacbb28bd7b161c02f55ba490143409a57d1d
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-25 20:18:38 -0600

    add support for about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  # Action to handle the About view
+  def about
+  end
+
+  # Action to handle the Home view
   def home
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,13 +1,22 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get the about page" do
+    get about_url
+    assert_response :success
+    assert_select "title", "About"
+  end
+
   test "should get the home page" do
     get home_url
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "root route should get the home page" do
     get root_url
     assert_response :success
+    assert_select "title", "Home"
   end
 end
